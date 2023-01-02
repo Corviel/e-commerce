2437b90dad768c0be9c6226b417088297caedaa2
Corviel <gabrielbgmenezes@gmail.com>
2023-01-02 18:53:34 -0300

    fix: :bug: Fixing routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Route, BrowserRouter as Router, Switch } from "react-router-dom"
+import { Route, HashRouter as Router, Switch } from "react-router-dom"
 import { Footer, Navbar, Sidebar } from "./components"
 import {
   About,
